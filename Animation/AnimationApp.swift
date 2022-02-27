e66c76390cc5eca57629e7e632c7545929ec1301
Garret Poole <garretpoole@users.noreply.github.com>
2022-02-27 01:40:46 -0800

    Initial Commit

---
+++ Animation/AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by Garret Poole on 2/27/22.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
