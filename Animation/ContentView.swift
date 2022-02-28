3ee8c78b929c8bed5c0deb87a52a12832fa8ee27
Garret Poole <garretpoole@users.noreply.github.com>
2022-02-28 08:17:19 -0800

    explicit animation on button press

--- Animation/ContentView.swift
+++ Animation/ContentView.swift
@@ -8,28 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var animationAmount = 1.0
+    @State private var animationAmount = 0.0
     
     var body: some View {
-        print(animationAmount)
-        
-        return VStack{
-            //former: state change has no idea itll animate the view bc implicit
-            //this case: the view has no idea itll be animated, the state change is what causes the animation to occur
-            Stepper("Scale Amount", value: $animationAmount.animation(
-                .easeInOut(duration: 1)
-                    .repeatCount(3, autoreverses: true)
-            ), in: 1...10)
-            Spacer()
-            Button("Tap Me") {
-                animationAmount += 1
+        Button("Tap Me") {
+            //just adding 360 will not do anything
+            withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)){
+                animationAmount += 360
             }
-            .padding(50)
-            .background(.red)
-            .foregroundColor(.white)
-            .clipShape(Circle())
-            .scaleEffect(animationAmount)
         }
+        .padding(50)
+        .background(.red)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
     }
 }
 
