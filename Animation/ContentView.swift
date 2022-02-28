a413af9df4a231d4a882f86454850a8c770822a3
Garret Poole <garretpoole@users.noreply.github.com>
2022-02-28 08:24:31 -0800

    modifier order matters, multiple animations

--- Animation/ContentView.swift
+++ Animation/ContentView.swift
@@ -8,15 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var animationAmount = 0.0
+    @State private var enabled = false
     
     var body: some View {
         Button("Tap Me") {
-            //do nothing
+            enabled.toggle()
         }
         .frame(width: 200, height: 200)
-        .background(.blue)
+        .background(enabled ? .blue : .red)
+        .animation(.default, value: enabled)
         .foregroundColor(.white)
+        .clipShape(RoundedRectangle(cornerRadius: enabled ? 60: 0))
+        .animation(.interpolatingSpring(stiffness: 10, damping: 1), value: enabled)
     }
 }
 
